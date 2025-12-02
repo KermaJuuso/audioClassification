707f02da7d1dfba2f33d7f7d20a39056bfe5ebdf
Juuso Toivanen <juuso.v.toivanen@tuni.fi>
2025-12-02 15:00:01 +0200

    extract features pohjaa.

--- extract_features.py
+++ extract_features.py
@@ -1,13 +1,33 @@
+import librosa
+import os
+from pydub import AudioSegment
+import numpy as np
+import soundfile as sf
+import json
 
-#Näihin kaikkiin varmaan jo valmis funktio
-def extract_RMS():
-    pass
 
-def extract_ZCR(): # Zero-crossing rate
-    pass
+def extract_features(folder_path):
+    for filename in os.listdir(folder_path):
+        if filename.startswith("."):
+            continue
 
-def extarct_spectral_centroid():
-    pass
+        path = os.path.join(folder_path, filename)
+        x, sr = librosa.load(path, sr=None)
 
-def extract_mel_spectogram():
-    pass
+
+        # Extract MFCCs
+        mfcc = librosa.feature.mfcc(y=x, sr=sr, n_mfcc=20)
+        mfcc_mean = mfcc.mean(axis=1) # Näitä tulee n_mfcc määrään mukaa
+        mfcc_std = mfcc.std(axis=1)
+
+        # Extract Spectral Centroid
+        scent = librosa.feature.spectral_centroid(y=x, sr=sr)
+        scent_mean = scent.mean()
+        scent_std = scent.std()
+
+        # Extract RMS energy
+        rms = librosa.feature.rms(y=x)
+        rms_mean = rms.mean()
+        rms_std = rms.std()
+
+        # Sitte jotenki jonkunlaisee muotoo et pystyy kouluttaa, vaikka CSV tai pickle tiedostoo tai numpy tiedosto
