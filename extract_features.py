dae7eeadac4cb9546a5ab4594d457ad61523605e
Juuso Toivanen <juuso.v.toivanen@tuni.fi>
2025-12-12 17:17:11 +0200

    Extract features finished and csv file created

--- extract_features.py
+++ extract_features.py
@@ -4,9 +4,12 @@ from pydub import AudioSegment
 import numpy as np
 import soundfile as sf
 import json
+import pandas as pd
 
 
-def extract_features(folder_path):
+def extract_features(folder_path, label):
+    rows = []
+
     for filename in os.listdir(folder_path):
         if filename.startswith("."):
             continue
@@ -31,3 +34,25 @@ def extract_features(folder_path):
         rms_std = rms.std()
 
         # Sitte jotenki jonkunlaisee muotoo et pystyy kouluttaa, vaikka CSV tai pickle tiedostoo tai numpy tiedosto
+        row = {"filename": filename,
+               "label": label}
+        
+        for i in range(20):
+            row[f"mfcc_{i+1}_mean"] = mfcc_mean[i]
+            row[f"mfcc_{i+1}_std"]  = mfcc_std[i]
+
+       
+        row["scent_mean"] = scent_mean
+        row["scent_std"] = scent_std
+        row["rms_mean"] = rms_mean
+        row["rms_std"] = rms_std
+
+        rows.append(row)
+
+    return rows
+
+bus_rows = extract_features("processed_data/bus", label="bus")
+car_rows = extract_features("processed_data/car", label="car")
+
+df = pd.DataFrame(bus_rows + car_rows)
+df.to_csv("features.csv", index=False)
